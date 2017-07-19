3d0bc1f8ab944774444aa7270ef5598503807727
Barry Frost <public@barryfrost.com>
2017-07-19 16:37:05 +0100

    RSVP webmentions now supported. Content updated.

---
+++ assets/webmentions.js
@@ -0,0 +1,55 @@
+$(function(){
+
+  function findRSVPs(data) {
+    var rsvps = { "rsvps": [] }
+    for (var i=0; i<data['links'].length; i++) {
+      var link = data['links'][i];
+      if (link['activity']['type'] == 'rsvp') {
+        var rsvp = {
+          "rsvp": link['data']['rsvp'],
+          "url": link['data']['url'],
+          "author": {
+            "name": link['data']['author']['name'],
+            "photo": link['data']['author']['photo']
+          }
+        }
+        switch (link['data']['rsvp']) {
+          case 'yes':
+            rsvp['icon'] = 'fa-calendar-check-o';
+            rsvp['label'] = 'is coming.';
+            break;
+          case 'no':
+            rsvp['icon'] = 'fa-calendar-times-o';
+            rsvp['label'] = 'is not coming.';
+            break;
+          case 'maybe':
+            rsvp['icon'] = 'fa-calendar-minus-o';
+            rsvp['label'] = 'may be coming.';
+            break;
+          case 'interested':
+            rsvp['icon'] = 'fa-calendar-o';
+            rsvp['label'] = 'is interested.';
+            break;
+        }
+        rsvps['rsvps'].push(rsvp);
+      }
+    }
+    return rsvps;
+  }
+
+  function buildRSVPs(rsvps) {
+    var template = $('#template').html();
+    var rendered = Mustache.render(template, rsvps);
+    $('#webmentions').html(rendered);
+  }
+
+  if ($('#webmentions').length) {
+    $.getJSON("https://webmention.io/api/mentions?jsonp=?", {
+      target: PAGE_URL
+    }, function(data) {
+      var rsvps = findRSVPs(data);
+      buildRSVPs(rsvps);
+    });
+  }
+
+});
