6c82ae575308a6173248134639533c9fd3d51eea
Barry Frost <public@barryfrost.com>
2017-08-23 11:45:55 +0100

    Use Vue.js for RSVPs rendering

---
+++ assets/webmentions-vue.js
@@ -0,0 +1,16 @@
+new Vue({
+
+    el: '#webmentions',
+
+    data: {
+      rsvps: []
+    },
+
+    mounted() {
+      const pageUrl = document.querySelectorAll("link[rel~=canonical]")[0].href
+      const apiUrl = "https://webmention.io/api/mentions?jsonp&target="
+      axios.get(apiUrl + encodeURIComponent(pageUrl))
+        .then(response => this.rsvps = response.data.links)
+    }
+
+  })
