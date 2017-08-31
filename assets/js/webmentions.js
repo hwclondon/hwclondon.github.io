8ac9f9fce8a670cbd796e43645fa0335c691d7ab
Calum Ryan <hello@calumryan.com>
2017-08-31 22:31:16 +0100

    Amend assets

--- assets/js/webmentions.js
+++ assets/js/webmentions.js
@@ -9,10 +9,10 @@ new Vue({
     },
 
     mounted() {
-      const pageUrl = document.querySelectorAll("link[rel~=canonical]")[0].href
-      const apiUrl = "https://webmention.io/api/mentions?jsonp&target="
+      const pageUrl = document.querySelectorAll("link[rel~=canonical]")[0].href;
+      const apiUrl = "https://webmention.io/api/mentions?jsonp&target=";
       axios.get(apiUrl + encodeURIComponent(pageUrl))
-        .then(response => this.rsvps = response.data.links)
+        .then(response => this.rsvps = response.data.links);
     }
 
   })
